da2151d0f9446ca0399cd1d0f0d1aadf15b48912
Arno Cai <acai367@gmail.com>
2022-07-28 01:14:55 -0400

    Add hover option over individual grid cells

--- dommethods.js
+++ dommethods.js
@@ -5,3 +5,29 @@ for (let numberDivs = 0; numberDivs < 16*16; numberDivs++) {
     gridBox.classList.add("size");
     document.querySelector('#wrapper').appendChild(gridBox);
 }
+
+// Get all inner divs as a NodeList
+let gridCell = document.querySelectorAll(".size");
+
+// Convert inner divs NodeList to array
+let gridCellArray = Array.from(gridCell);
+
+// Function to call hover
+gridCellArray.forEach(miniCell => {
+    miniCell.addEventListener("mouseover", e => {
+        e.target.style.backgroundColor = "red";
+        // setTimeout(function() {
+        //     e.target.style.backgroundColor = "red"
+        // }, 100);
+    });
+
+    miniCell.addEventListener("mouseleave", e => {
+        e.target.style.backgroundColor = "white";
+        // setTimeout(function() {
+        //     e.target.style.backgroundColor = "white"
+        // }, 100);
+    });
+}); /* use an arrow function */
+
+
+/* NEXT STEPS: figure out how to leave the trail. also try to understand the css and js code you've written. it can be a lot to digest, but I think it is important to solidify your understanding of the fundamentals. */
